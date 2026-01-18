13a0a6ada00a92b47a750d057935f252924fb832
Jó Márkó <jomar@MacBook-Pro-9.local>
2026-01-18 07:45:03 +0100

    Updates

--- assets/index-CErqOb4w.js
+++ assets/index-CErqOb4w.js
@@ -1,4 +1,4 @@
-import{g as ql}from"./index-ICuu13Hv.js";/*! *****************************************************************************
+import{g as ql}from"./index-PtUwDsRo.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
