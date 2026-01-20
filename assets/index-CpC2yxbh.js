5ad2bca2026df97700f54dc056af1dd1d073ab50
Jó Márkó <jomar@MacBook-Pro-9.local>
2026-01-20 21:09:27 +0100

    Updates

--- assets/index-CpC2yxbh.js
+++ assets/index-CpC2yxbh.js
@@ -1,4 +1,4 @@
-import{g as ql}from"./index-CUkIaNLK.js";/*! *****************************************************************************
+import{g as ql}from"./index-HbtPiUbQ.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
