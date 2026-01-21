cf56c5edd6b49cd3863a73dc3aaab2c0be574ccb
Jó Márkó <jomar@MacBook-Pro-9.local>
2026-01-21 07:48:58 +0100

    Updates

--- assets/index-QKDMcSXD.js
+++ assets/index-QKDMcSXD.js
@@ -1,4 +1,4 @@
-import{g as ql}from"./index-MLtwqaRn.js";/*! *****************************************************************************
+import{g as ql}from"./index-Df0SK1Uo.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
